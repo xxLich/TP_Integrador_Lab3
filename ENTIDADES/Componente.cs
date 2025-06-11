1d50d3a5b18b3db1693502ad741a086812c2afe3
Juani-R08 <juani.rosales15@gmail.com>
2025-06-11 17:27:14 -0300

    Clases y cambio en el inicio
    Se definieron las clases para cada tabla
    Cambios en el login

---
+++ ENTIDADES/Componente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDADES
+{
+    class Componente
+    {
+
+		private string _Nombre;
+		private string _Descripcion;
+		private int _Categoria;
+		private decimal _PrecioVenta;
+		private decimal _PrecioCosto;
+		private int _Stock;
+		private DateTime _FechaCreacion;
+       
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+        public string Descripcion { get => _Descripcion; set => _Descripcion = value; }
+        public int Categoria { get => _Categoria; set => _Categoria = value; }
+        public decimal PrecioVenta { get => _PrecioVenta; set => _PrecioVenta = value; }
+        public decimal PrecioCosto { get => _PrecioCosto; set => _PrecioCosto = value; }
+        public int Stock { get => _Stock; set => _Stock = value; }
+        public DateTime FechaCreacion { get => _FechaCreacion; set => _FechaCreacion = value; }
+      }
+
+
+
+
+    }
+}
---
+++ ENTIDADES/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDADES
+{
+    class Usuario
+    {
+	private string _Nombre;
+		private string _Apellido;
+		private string _Email;
+		private string _Clave;
+		private DateTime _FechaRegistro;
+        private int _Rol;
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+        public string Apellido { get => _Apellido; set => _Apellido = value; }
+        public int Rol { get => _Rol; set => _Rol = value; }
+        public string Email { get => _Email; set => _Email = value; }
+        public DateTime FechaRegistro { get => _FechaRegistro; set => _FechaRegistro = value; }
+       
+
+
+
+
+    }
+}
