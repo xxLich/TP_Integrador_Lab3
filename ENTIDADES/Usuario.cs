cf28e42f6c48cbf64f42efdc92a01959f43bd32a
Juani-R08 <juani.rosales15@gmail.com>
2025-06-11 18:40:34 -0300

    clave usuario

--- ENTIDADES/Usuario.cs
+++ ENTIDADES/Usuario.cs
@@ -8,16 +8,17 @@ namespace ENTIDADES
 {
     class Usuario
     {
-	private string _Nombre;
-		private string _Apellido;
-		private string _Email;
-		private string _Clave;
-		private DateTime _FechaRegistro;
+        private string _Nombre;
+        private string _Apellido;
+        private string _Email;
+        private string _Clave;
+        private DateTime _FechaRegistro;
         private int _Rol;
         public string Nombre { get => _Nombre; set => _Nombre = value; }
         public string Apellido { get => _Apellido; set => _Apellido = value; }
         public int Rol { get => _Rol; set => _Rol = value; }
         public string Email { get => _Email; set => _Email = value; }
+        public string Clave { get => _Clave; set => _Clave = value; }
         public DateTime FechaRegistro { get => _FechaRegistro; set => _FechaRegistro = value; }
        
 
