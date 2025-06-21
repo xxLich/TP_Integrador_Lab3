6c3e1431b461a33115234c48f5e2e738b5184777
Lisandro <98889906+xxLich@users.noreply.github.com>
2025-06-21 13:51:15 -0300

    Update TRIGGERS_TP.sql

--- TRIGGERS_TP.sql
+++ TRIGGERS_TP.sql
@@ -2,7 +2,6 @@ USE TP_ITEGRADOR_GP4
 GO
 
 -- ACTUALIZAR ESTADO COMPONENTE 
-
 CREATE TRIGGER ActualizarEstadoComponente
 ON Componentes
 AFTER UPDATE
@@ -10,21 +9,17 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Solo ejecutar si nivel de trigger es 1
-    IF TRIGGER_NESTLEVEL() > 1
-        RETURN;
-
-    -- Actualizar Estado a 0 si Stock = 0 y Estado <> 0
+    -- Actualizar Estado a 0 si Stock = 0
     UPDATE C
     SET C.Estado = 0
     FROM Componentes C
     INNER JOIN inserted I ON C.IDComponente = I.IDComponente
-    WHERE I.Stock = 0 AND C.Stock = 0 AND C.Estado = 1;
+    WHERE I.Stock = 0 AND C.Estado = 1;
 
-    -- Actualizar Estado a 1 si Stock > 0 y Estado <> 1
+    -- Actualizar Estado a 1 si Stock > 0
     UPDATE C
     SET C.Estado = 1
     FROM Componentes C
     INNER JOIN inserted I ON C.IDComponente = I.IDComponente
-    WHERE I.Stock > 0 AND C.Stock > 0 AND C.Estado <> 1;
+    WHERE I.Stock > 0 AND C.Estado = 0;
 END;
